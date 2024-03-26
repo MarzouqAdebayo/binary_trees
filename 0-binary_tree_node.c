aec91aaaf724cc52db60495e626290c95e941e0a
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-26 09:27:24 +0000

    Completed task one, binary_tree_node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ *
+ * @parent: the parent node of the binary tree
+ * @value: the data value for the node
+ * Return: the newly created node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *root;
+
+	root = malloc(sizeof(binary_tree_t));
+	if (!root)
+		return NULL;
+	root->n = value;
+	root->parent = parent;
+	root->left = NULL;
+	root->right = NULL;
+
+	return (root);
+}
