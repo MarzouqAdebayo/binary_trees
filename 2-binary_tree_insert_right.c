08147f0f72a3e8c5de9e6f8ebe2e9bfa5578aab4
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-27 03:44:45 +0000

    Fixed bug in task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,14 +11,23 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_right_child;
 
+	if (!parent)
+		return (NULL);
+
 	new_right_child = malloc(sizeof(binary_tree_t));
 	if (!new_right_child)
 		return (NULL);
-	new_right_child->parent = parent,
 	new_right_child->n = value;
-	new_right_child->right = parent->right;
+	new_right_child->parent = parent,
 	new_right_child->left = NULL;
-	parent->right = new_right_child;
+	if (parent->right)
+	{
+		parent->right->parent = new_right_child;
+		new_right_child->right = parent->right;
+		parent->right = new_right_child;
+	}
+	else
+		parent->left = new_right_child;
 
 	return (new_right_child);
 }
