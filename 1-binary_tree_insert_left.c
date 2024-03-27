6ba60b33a239df5acb13d60d024cb389c4bdb45d
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-27 03:44:10 +0000

    Fixed bug in task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,15 +11,23 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_child;
 
+	if (!parent)
+		return (NULL);
+
 	new_left_child = malloc(sizeof(binary_tree_t));
 	if (!new_left_child)
 		return (NULL);
 	new_left_child->parent = parent,
 	new_left_child->n = value;
-	new_left_child->left = parent->left;
 	new_left_child->right = NULL;
-	parent->left = new_left_child;
+	if (parent->left)
+	{
+		parent->left->parent = new_left_child;
+		new_left_child->left = parent->left;
+		parent->left = new_left_child;
+	}
+	else
+		parent->left = new_left_child;
 
 	return (new_left_child);
 }
-
