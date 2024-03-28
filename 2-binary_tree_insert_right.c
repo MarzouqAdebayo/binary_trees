809ac924dfe4b84e356199e2ce48f86efb2b84a4
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-28 00:39:24 +0000

    Fixed bugs in tasks 1 and 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_right_child->n = value;
 	new_right_child->parent = parent,
 	new_right_child->left = NULL;
+	new_right_child->rigt = NULL;
 	if (parent->right)
 	{
 		parent->right->parent = new_right_child;
